ee97794e349ad8b858c3fdf374bbdff9baec6873
Patryk <cien2460045@gmail.com>
2021-11-03 18:06:42 +0100

    First changes in files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			{/* header */}
+			<header className="header">
+				<h1>Football Highlights</h1>
+			</header>
+			{/* main */}
+			<main className="main"></main>
+		</div>
+	);
 }
 
 export default App;
